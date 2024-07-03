c9feeb68c73318022372cfe61991818c503ce385
multinett <72457258+multinettt@users.noreply.github.com>
2024-07-03 22:10:36 +0300

    Bismillahirahmanirrahim.

---
+++ lua/arc9/shared/sh_arc9_bocw_assaultriflesdlc_sounds.lua
@@ -0,0 +1,337 @@
+--=============================================================================
+--   CALL OF DUTY: BLACK OPS COLD WAR
+--   Assault Rifles DLC Sounds Master File
+--   LUA - SOUNDS
+
+--========== QBZ-83 ===========================================================
+-- MAGAZINE
+
+sound.Add({
+    name = "ARC9_BOCW.Groza_reload_magout",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_groza/groza_reload_magout.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.Groza_reload_magin",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_groza/groza_reload_magin.wav"
+})
+
+
+sound.Add({
+    name = "ARC9_BOCW.Groza_reload_ext_magout",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_groza/groza_reload_ext_magout.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.Groza_reload_ext_magin",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_groza/groza_reload_ext_magin.wav"
+})
+
+-- BOLT OPERATION
+
+sound.Add({
+    name = "ARC9_BOCW.Groza_boltback",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_groza/groza_boltback.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.Groza_boltrelease",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_groza/groza_boltrelease.wav"
+})
+
+-- RELOAD FOLEY
+
+sound.Add({
+    name = "ARC9_BOCW.Groza_reload_start",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_groza/groza_reload_start.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.Groza_reload_end",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_groza/groza_reload_end.wav"
+})
+
+-- INSPECT FOLEY
+
+sound.Add({
+    name = "ARC9_BOCW.Groza_inspect_part1",
+    channel = CHAN_ITEM,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_groza/groza_inspect_part1.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.Groza_inspect_part2",
+    channel = CHAN_ITEM,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_groza/groza_inspect_part2.wav"
+})
+
+
+sound.Add({
+    name = "ARC9_BOCW.Groza_inspect_fire_part1",
+    channel = CHAN_ITEM,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_groza/groza_inspect_fire_part1.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.Groza_inspect_fire_part2",
+    channel = CHAN_ITEM,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_groza/groza_inspect_fire_part2.wav"
+})
+
+-- PULLOUT PUTAWAY
+
+sound.Add({
+    name = "ARC9_BOCW.Groza_draw",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_groza/groza_draw.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.Groza_holster",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_groza/groza_holster.wav"
+})
+
+-----------------------------------------------
+-- FIRING SOUNDS
+
+sound.Add({ -- BLACK OPS COLD WAR
+    name = "ARC9_BOCW.Groza_fire",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    level = 100,
+    sound = "weapons/arc9/bocw_groza/groza_fire.wav"
+})
+
+sound.Add({ -- WARZONE
+    name = "ARC9_BOCW.Groza_fire_wz",
+    channel = CHAN_STATIC,
+    volume = 0.7,
+    level = 100,
+    sound = "weapons/arc9/bocw_groza/groza_fire_wz.wav"
+})
+--[[
+sound.Add({ -- BLACK OPS
+    name = "ARC9_BOCW.Groza_fire_bo",
+    channel = CHAN_STATIC,
+    volume = 1.0,
+    level = 100,
+    sound = { "weapons/arc9/bocw_groza/groza_fire_bo_1.wav", "weapons/arc9/bocw_groza/groza_fire_bo_2.wav","weapons/arc9/bocw_groza/groza_fire_bo_3.wav", "weapons/arc9/bocw_groza/groza_fire_bo_4.wav", "weapons/arc9/bocw_groza/groza_fire_bo_5.wav" }
+})
+]]
+sound.Add({
+    name = "ARC9_BOCW.Groza_fire_silenced",
+    channel = CHAN_STATIC,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_groza/groza_fire_silenced.wav"
+})
+
+--========== FFAR1 ============================================================
+-- MAGAZINE
+
+sound.Add({
+    name = "ARC9_BOCW.FFAR1_reload_magout",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_ffar1/ffar1_reload_magout.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.FFAR1_reload_magin",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_ffar1/ffar1_reload_magin.wav"
+})
+
+
+sound.Add({
+    name = "ARC9_BOCW.FFAR1_reload_empty_magout",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_ffar1/ffar1_reload_empty_magout.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.FFAR1_reload_empty_magin",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_ffar1/ffar1_reload_empty_magin.wav"
+})
+
+-- BOLT OPERATION
+
+sound.Add({
+    name = "ARC9_BOCW.FFAR1_boltback",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_ffar1/ffar1_boltback.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.FFAR1_boltrelease",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_ffar1/ffar1_boltrelease.wav"
+})
+
+-- RELOAD FOLEY
+
+sound.Add({
+    name = "ARC9_BOCW.FFAR1_reload_start",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_ffar1/ffar1_reload_start.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.FFAR1_ready_end",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_ffar1/ffar1_ready_end.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.FFAR1_reload_end",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_ffar1/ffar1_reload_end.wav"
+})
+
+-- INSPECT FOLEY
+
+sound.Add({
+    name = "ARC9_BOCW.FFAR1_inspect_part1",
+    channel = CHAN_ITEM,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_ffar1/ffar1_inspect_part1.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.FFAR1_inspect_part2",
+    channel = CHAN_ITEM,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_ffar1/ffar1_inspect_part2.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.FFAR1_inspect_music",
+    channel = CHAN_ITEM,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_ffar1/ffar1_inspect_music.wav"
+})
+
+-- PULLOUT PUTAWAY
+
+sound.Add({
+    name = "ARC9_BOCW.FFAR1_draw",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_ffar1/ffar1_draw.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.FFAR1_holster",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_ffar1/ffar1_holster.wav"
+})
+
+-----------------------------------------------
+-- FIRING SOUNDS
+
+sound.Add({ -- BLACK OPS COLD WAR
+    name = "ARC9_BOCW.FFAR1_fire",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    level = 100,
+    sound = "weapons/arc9/bocw_ffar1/ffar1_fire.wav"
+})
+
+sound.Add({ -- WARZONE
+    name = "ARC9_BOCW.FFAR1_fire_wz",
+    channel = CHAN_STATIC,
+    volume = 0.5,
+    level = 100,
+    sound = "weapons/arc9/bocw_ffar1/ffar1_fire_wz.wav"
+})
+--[[
+sound.Add({ -- BLACK OPS
+    name = "ARC9_BOCW.FFAR1_fire_bo",
+    channel = CHAN_STATIC,
+    volume = 1.0,
+    level = 100,
+    sound = { "weapons/arc9/bocw_ffar1/ffar1_fire_bo_1.wav", "weapons/arc9/bocw_ffar1/ffar1_fire_bo_2.wav","weapons/arc9/bocw_ffar1/ffar1_fire_bo_3.wav", "weapons/arc9/bocw_ffar1/ffar1_fire_bo_4.wav", "weapons/arc9/bocw_ffar1/ffar1_fire_bo_5.wav" }
+})
+]]
+sound.Add({
+    name = "ARC9_BOCW.FFAR1_fire_silenced",
+    channel = CHAN_STATIC,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_ffar1/ffar1_fire_silenced.wav"
+})
+
+--[[---------------------------------------------------------------------------------
+
+3.07.2024
+
+I am a changed man.
+
+After the 2024 YKS, I feel robbed, feel... different. I did all I could though, thats
+the thing that actually matters there. Hoping for the best.
+
+After not doing this type of stuff for -an actually not that long amount of time- it
+actually feels like a chore honestly! I really don't know why, really don't. I simply
+appear to have lost more than half of my muscle memory. Like a viewmodel is usually done in
+like 30 minutes from raw files. It took me like hours in multiple days for me to get the
+Groza viewmodel done. Part of me thinks this is just due to me losing my form and morale
+by stopping to do it over and over and over again. Like i never felt it being this tiring
+before.
+
+It's been a little over half a year, and I am still in shock that in just seven months
+ive lost so much of interest to this stuff and GMOD. I believe myself to be a mentally
+strong person but still, that fucking shitshow of an exam may have shorted some wires
+in my mind or something.
+
+So yeah, today ive decided to start work on the assault rifles dlc since its been so
+damn long and i do want to kinda get my shit together. When youre not making a buck out
+of something it is really difficult to move the ass. The sole reason i even started this
+whole workshop of mine is so the people can enjoy the stuff i managed to be able to make,
+like im a positive person, i believe in the good of people that they deserve and should
+be able to use the stuff i make and have fun with it. Thats honestly it. Completionism
+could also come into play but since im not even CLOSE to halfway done thats out the window
+instantly. I made a ton of progress in ArcCW but when ARC9 released i simply decided that
+arccw was a completely inferior base comparatively and my weapons there were too old. I
+made the decision to start fresh, as i found a way to do stuff in my way specifically that
+is way better than the old method i used.
+
+Ok, what should you take of this? I went through shit and trying to recover. This ars dlc
+may take a really long time, there are a lot of guns and i need to get my shit together to
+make them in the first place.
+
+Watch yourself.
+
+-Emirhan
+
+]]
