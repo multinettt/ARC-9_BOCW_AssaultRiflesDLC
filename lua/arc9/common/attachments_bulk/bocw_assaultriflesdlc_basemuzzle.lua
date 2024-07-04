9f32e2f91c6674a2d4711188e4268a07e41124e8
multinett <72457258+multinettt@users.noreply.github.com>
2024-07-04 13:28:23 +0300

    Groza is usable now

---
+++ lua/arc9/common/attachments_bulk/bocw_assaultriflesdlc_basemuzzle.lua
@@ -0,0 +1,25 @@
+local ATT = {}
+
+ATT = {}
+
+ATT.PrintName = "Groza Muzzle Brake"
+ATT.CompactName = "GROZA MUZZ"
+ATT.Icon = Material("entities/bocw_atts/muzzles/groza_base.png", "mips smooth")
+ATT.Description = [[5.56x45mm Muzzle Brake]]
+
+ATT.SortOrder = -10
+
+ATT.Model = "models/weapons/arc9/atts/bocw_groza_muzzle.mdl"
+
+ATT.Category = {"bocw_groza_muzzle"}
+
+ATT.ModelOffset = Vector(0, 0, 0)
+ATT.ModelAngleOffset = Angle(0, 0, 0)
+
+ATT.MuzzleParticleOverride = "muzzleflash_2"
+ATT.MuzzleParticleOverride_Priority = 10
+ATT.MuzzleDevice = true
+
+ATT.MenuCategory = "ARC9 - BOCW Attachments"
+
+ARC9.LoadAttachment(ATT, "bocw_groza_muzzle_base")
